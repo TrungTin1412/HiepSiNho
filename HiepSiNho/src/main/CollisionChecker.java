d4f1f3f63aa44c0ec437f8a163a9ad4a89792f5e
CS Zone <shinobikachimiku@gmail.com>
2023-05-23 16:25:09 +0700

    clear code tree

--- HiepSiNho/src/main/CollisionChecker.java
+++ HiepSiNho/src/main/CollisionChecker.java
@@ -1,5 +1,6 @@
 package main;
 
+
 import entity.Entity;
 
 public class CollisionChecker {
---
+++ HiepSiNho/src/object/OJB_BladeShot.java
@@ -0,0 +1,30 @@
+// package object;
+
+
+// //Entity -> Projectile -> BladeShot
+
+
+// import entity.Projectile;
+// import main.GamePanel;
+
+// public class OJB_BladeShot extends Projectile{
+	
+// 	GamePanel gp;
+
+// 	public OJB_BladeShot(GamePanel gp) {
+// 		super(gp);
+// 		this.gp = gp;
+		
+// 	    name = "BladeShot";
+// 		speed = 5; 
+// 		maxLife = 80;
+// 		life = maxLife;
+// 		alive = false;
+// 		getImage();	
+// 	}
+	
+// 	public void getImage() {
+		
+// 	}
+
+// }
